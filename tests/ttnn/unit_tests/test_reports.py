183c8d874fe6a5bf1ff475516507e35889e09b99
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2024-03-30 19:56:44 +0000

    #6644: updated l1 visualizer

--- tests/ttnn/unit_tests/test_reports.py
+++ tests/ttnn/unit_tests/test_reports.py
@@ -64,7 +64,7 @@ Core: (x=10,y=9)
 
 @pytest.mark.parametrize("height", [1024 * 5])
 @pytest.mark.parametrize("width", [1024 * 2])
-def test_enable_l1_buffers_logging(tmp_path, height, width):
+def test_enable_buffer_report(tmp_path, height, width):
     ttnn.ENABLE_LOGGING = True
     ttnn.ENABLE_BUFFER_REPORT = True
 
@@ -92,3 +92,42 @@ def test_enable_l1_buffers_logging(tmp_path, height, width):
 
     ttnn.ENABLE_LOGGING = False
     ttnn.ENABLE_BUFFER_REPORT = False
+
+    sqlite_connection = ttnn.database.get_or_create_sqlite_db(ttnn.database.DATABASE_FILE)
+    cursor = sqlite_connection.cursor()
+    cursor.execute("SELECT * FROM buffer_pages")
+    buffer_pages = []
+    for row in cursor.fetchall():
+        buffer_pages.append(row)
+    assert len(buffer_pages) > 0
+
+
+@pytest.mark.parametrize("height", [1024])
+@pytest.mark.parametrize("width", [1024])
+def test_enable_tensor_report(tmp_path, height, width):
+    ttnn.ENABLE_LOGGING = True
+    ttnn.ENABLE_TENSOR_REPORT = True
+
+    torch.manual_seed(0)
+
+    device = ttnn.open_device(device_id=0)
+
+    torch_input_tensor = torch.rand(
+        (height, width),
+        dtype=torch.bfloat16,
+    )
+
+    input_tensor_a = ttnn.from_torch(
+        torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device, memory_config=ttnn.L1_MEMORY_CONFIG
+    )
+
+    input_tensor_b = ttnn.from_torch(
+        torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device, memory_config=ttnn.L1_MEMORY_CONFIG
+    )
+    output_tensor = ttnn.add(input_tensor_a, input_tensor_b, memory_config=ttnn.L1_MEMORY_CONFIG)
+    ttnn.to_torch(output_tensor)
+
+    device = ttnn.close_device(device)
+
+    ttnn.ENABLE_LOGGING = False
+    ttnn.ENABLE_TENSOR_REPORT = False
