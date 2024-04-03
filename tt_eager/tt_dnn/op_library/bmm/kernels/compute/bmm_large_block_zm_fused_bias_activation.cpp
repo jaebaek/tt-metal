a8a964969259cd1d57fa574fae680fbbbe5a9d0e
yugaoTT <yugao@tenstorrent.com>
2024-04-03 09:25:53 -0500

    #0: repro hang

--- tt_eager/tt_dnn/op_library/bmm/kernels/compute/bmm_large_block_zm_fused_bias_activation.cpp
+++ tt_eager/tt_dnn/op_library/bmm/kernels/compute/bmm_large_block_zm_fused_bias_activation.cpp
@@ -8,7 +8,8 @@
 #include "compute_kernel_api/tile_move_copy.h"
 #include "compute_kernel_api/matmul.h"
 #include "compute_kernel_api/pack_untilize.h"
-//#include "debug/dprint.h"
+#include "debug/dprint.h"
+#include "debug/status.h"
 
 #ifdef FUSE_BIAS
 #include "compute_kernel_api/bcast.h"
@@ -86,6 +87,8 @@ void MAIN {
     constexpr uint32_t out_block_num_tiles = get_compile_time_arg_val(12); // number of tiles in out_block
     constexpr bool untilize_out = get_compile_time_arg_val(13); // untilize output
 
+    UNPACK(( DPRINT  << "num_blocks " << num_blocks << ENDL()  ));
+
     constexpr uint32_t out_block_w = out_subblock_w*in1_num_subblocks;
 
     constexpr uint32_t in0_cb_id = tt::CB::c_in0;
@@ -135,8 +138,8 @@ void MAIN {
             }
             #endif
 
-            cb_wait_front(in0_cb_id, in0_block_num_tiles);
-            cb_wait_front(in1_cb_id, in1_block_num_tiles);
+            // cb_wait_front(in0_cb_id, in0_block_num_tiles);
+            // cb_wait_front(in1_cb_id, in1_block_num_tiles);
             int in0_index_subblock_offset = 0;
             for (uint32_t in0_subblock = 0; in0_subblock < in0_num_subblocks; in0_subblock++) {
                 int in1_index_subblock_offset = 0;
@@ -146,7 +149,11 @@ void MAIN {
                         reload_from_cb_to_dst(in0_cb_id, in1_cb_id, mm_partials_cb_id, out_subblock_num_tiles, out_subblock_w, out_subblock_h, in0_block_w);
                     } else {
                         // just acquire
+
+                        DEBUG_STATUS('o');
                         tile_regs_acquire();
+
+                        DEBUG_STATUS('p');
                     }
 
                     // Compute output sub-block
@@ -154,6 +161,7 @@ void MAIN {
                     uint32_t in0_index = in0_index_subblock_offset; // offset into in0 block
                     uint32_t in1_index = in1_index_subblock_offset; // offset into in1 block
                     // inner dim that we accumualte is the inner dim of in0/in1, which is in0_block_w
+                    DEBUG_STATUS('i');
                     for (uint32_t inner_dim_idx = 0; inner_dim_idx < in0_block_w; ++inner_dim_idx) {
                         // matmul outer product of (out_subblock_h x out_subblock_w) tiles that fill dst
                         // accumulation is done by iterating matmul_block across inner dim
@@ -162,6 +170,7 @@ void MAIN {
                         in0_index ++;  // stride right by 1
                         in1_index += in1_per_core_w; // to stride down by 1 need to stride by in_per_core_w (should be called in1_block_w)
                     }
+                    DEBUG_STATUS('j');
 
                     if (last_out) {
                         // If we fuse bias, we will pack out and run bias + optional sfpu in a separate loop
@@ -175,6 +184,8 @@ void MAIN {
                         cb_reserve_back(mm_out_cb_id, out_subblock_num_tiles);
                         tile_regs_wait();
 
+                        DEBUG_STATUS('a');
+
                         #ifdef PACKER_L1_ACC
                             #ifdef FUSE_BIAS
                                 if (block == 0) { // no accumulation for first iteration
@@ -187,6 +198,8 @@ void MAIN {
                             #endif
                         #endif
 
+                        DEBUG_STATUS('b');
+
                         #if defined FP32_DEST_ACC_EN or defined PACKER_L1_ACC
                             PACK((  pack_reconfig_data_format(mm_out_cb_id) ));
                         #endif
@@ -196,6 +209,9 @@ void MAIN {
 
                         tile_regs_release();
                         cb_push_back(mm_out_cb_id, out_subblock_num_tiles);
+                        // cb_wait_front(mm_out_cb_id, out_subblock_num_tiles);
+
+                        DEBUG_STATUS('c');
                     } else {
                         tile_regs_commit();
                         // Wait for tiles in output buffer to be written out since interm and output share memory
@@ -207,6 +223,8 @@ void MAIN {
                         cb_reserve_back(mm_partials_cb_id, out_subblock_num_tiles);
                         tile_regs_wait();
 
+                        DEBUG_STATUS('d');
+
                         #ifdef PACKER_L1_ACC
                             if (block == 0) { // no accumulation for first iteration
                                 PACK((  llk_pack_reconfig_l1_acc(0) ));
@@ -215,17 +233,30 @@ void MAIN {
                             }
                         #endif
 
+                        DEBUG_STATUS('e');
+
                         uint32_t start_dst_index = 0;
                         matmul_pack_tile(start_dst_index, mm_partials_cb_id, out_subblock_num_tiles);
 
                         tile_regs_release();
                         cb_push_back(mm_partials_cb_id, out_subblock_num_tiles);
+                        // cb_wait_front(mm_partials_cb_id, out_subblock_num_tiles);
+
+                        // UNPACK(( DPRINT  << "block " << block << ENDL()  ));
+
+                        DEBUG_STATUS('f');
                     }
 
                     in1_index_subblock_offset += out_subblock_w;
+
+                    DEBUG_STATUS('m');
                 }
                 in0_index_subblock_offset += in0_subblock_num_tiles;
+
+                DEBUG_STATUS('n');
             }
+
+            DEBUG_STATUS('g');
 
             #ifdef PACKER_L1_ACC
                 #ifdef FUSE_BIAS
@@ -242,6 +273,8 @@ void MAIN {
             #else
                 if (spill) enable_reload = true;
             #endif
+
+            DEBUG_STATUS('h');
 
             cb_pop_front(in0_cb_id, in0_block_num_tiles);
             cb_pop_front(in1_cb_id, in1_block_num_tiles);
