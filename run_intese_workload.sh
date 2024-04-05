c2c63199b97b54cc058f93ea871df61f82cbd5bd
Milos Trajkovic <mtrajkovic@tenstorrent.com>
2024-04-05 15:45:18 +0000

    #0: identical dims to failing wload; run tighter test loop in python

--- run_intese_workload.sh
+++ run_intese_workload.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/env bash
 NUM_LOOPS=$1
 
-for ((i=0; i<${NUM_LOOPS}; i++))
-do
-	pytest -svv tests/tt_eager/python_api_testing/unit_testing/misc/test_matmul_1d_2d.py::test_multi_core_matmul_2d_8x8[False-True-True-2048-2048-4096-None-dtype5-fidelity5-True-False-no_bias]
-done
+	WL_NUM_LOOPS=${NUM_LOOPS} pytest -svv tests/tt_eager/python_api_testing/unit_testing/misc/test_matmul_1d_2d.py::test_loop_multi_core_matmul_2d_8x8[False-True-True-3072-2048-4096-None-dtype0-fidelity0-True-False-no_bias]
