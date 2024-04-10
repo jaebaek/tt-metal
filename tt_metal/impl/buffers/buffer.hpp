8ec6e1610c5fc1b5fadaecc5a7dd9a3d1cb4c114
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2024-04-10 02:07:04 +0000

    #4003: added reflection for every object in memory config

--- tt_metal/impl/buffers/buffer.hpp
+++ tt_metal/impl/buffers/buffer.hpp
@@ -60,8 +60,12 @@ struct ShardSpec {
 
     const uint32_t num_cores() const { return this->grid.num_cores(); }
     const uint32_t numel() const { return this->shape[0] * this->shape[1]; }
-    tt::stl::reflection::Attributes attributes() const;
 
+    static constexpr auto attribute_names = std::make_tuple("grid", "shape", "orientation", "halo");
+    const auto attribute_values() const {
+        return std::make_tuple(
+            std::cref(this->grid), std::cref(this->shape), std::cref(this->orientation), std::cref(this->halo));
+    }
 };
 
 bool operator==(const ShardSpec& spec_a, const ShardSpec& spec_b);
