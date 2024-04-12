c52cae3d333d35faf1743bb1c97f03d79c4b55d2
Djordje Ivanovic <divanovic@tenstorrent.com>
2024-04-12 00:30:16 +0000

    #5592: Fix weight reusing between MLP prefill and decode

--- models/demos/falcon7b/tt/falcon_mlp.py
+++ models/demos/falcon7b/tt/falcon_mlp.py
@@ -298,6 +298,7 @@ class TtFalconMLPDecode(nn.Module):
 
         custom_output_shape_h_to_4h = (
             (1, 1, self.padding_value, 4 * self.padding_value) if self.prefill_seq_len in [1024, 2048] else None
+        )
         custom_output_shape_4h_to_h = (
             (1, 1, 4 * self.padding_value, self.padding_value) if self.prefill_seq_len in [1024, 2048] else None
         )
