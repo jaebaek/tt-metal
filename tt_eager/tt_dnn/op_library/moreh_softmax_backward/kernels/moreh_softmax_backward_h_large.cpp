1835249560f62b9cb9f74f305703517ad2c24366
hschoi <hschoi4448@gmail.com>
2024-04-24 12:15:58 +0000

    #7521: remove moreh helper functions

--- tt_eager/tt_dnn/op_library/moreh_softmax_backward/kernels/moreh_softmax_backward_h_large.cpp
+++ tt_eager/tt_dnn/op_library/moreh_softmax_backward/kernels/moreh_softmax_backward_h_large.cpp
@@ -7,11 +7,15 @@
 #define REDUCE_OP PoolType::SUM
 #define REDUCE_DIM ReduceDim::REDUCE_COL
 
-#include "tt_eager/tt_dnn/kernels/compute/moreh_common.hpp"
+#include "compute_kernel_api/eltwise_binary.h"
+#include "compute_kernel_api/reduce.h"
+#include "compute_kernel_api/tile_move_copy.h"
 
 namespace NAMESPACE {
 void MAIN {
     constexpr uint32_t onetile = 1;
+    constexpr int dst0 = 0;
+    constexpr int dst_mask = 1;
 
     constexpr auto cb_y = tt::CB::c_in0;
     constexpr auto cb_dy = tt::CB::c_in1;
@@ -33,16 +37,21 @@ void MAIN {
         // This for loop should not affect the result.
         for (uint32_t h = 0; h < Ht; ++h) {
             if (h == 0) {
-                ACQ();
-                copy_tile_to_cb(cb_dy, cb_add);
-                REL();
+                acquire_dst(tt::DstMode::Half);
+                cb_reserve_back(cb_add, onetile);
+                cb_wait_front(cb_dy, 1);
+
+                copy_tile_init();
+                copy_tile(cb_dy, 0, dst0);
+                pack_tile(dst0, cb_add);
+
+                cb_pop_front(cb_dy, onetile);
+                cb_push_back(cb_add, onetile);
+                release_dst(tt::DstMode::Half);
             }
             else {
                 if (h == Ht - 1) {
-                    ACQ();
-                    constexpr uint32_t onetile = 1;
-                    constexpr int dst0 = 0;
-                    constexpr int dst_mask = 1;
+                    acquire_dst(tt::DstMode::Half);
 
                     cb_reserve_back(cb_inter0, onetile);
                     cb_wait_front(cb_dy, onetile);
@@ -60,35 +69,97 @@ void MAIN {
 
                     cb_push_back(cb_inter0, onetile);
 
-                    REL();
+                    release_dst(tt::DstMode::Half);
 
-                    ACQ();
-                    add_tiles_to_cb(cb_add, cb_inter0, cb_add);
-                    REL();
+                    acquire_dst(tt::DstMode::Half);
+                    cb_reserve_back(cb_add, onetile);
+                    cb_wait_front(cb_add, onetile);
+                    cb_wait_front(cb_inter0, onetile);
+
+                    add_tiles_init();
+                    add_tiles(cb_add, cb_inter0, 0, 0, dst0);
+
+                    pack_tile(dst0, cb_add);
+
+                    cb_pop_front(cb_add, onetile);
+                    cb_pop_front(cb_inter0, onetile);
+
+                    cb_push_back(cb_add, onetile);
+                    release_dst(tt::DstMode::Half);
                 } else {
-                    ACQ();
-                    copy_tile_to_cb(cb_dy, cb_add);
-                    REL();
+                    acquire_dst(tt::DstMode::Half);
+
+                    cb_reserve_back(cb_add, onetile);
+                    cb_wait_front(cb_dy, 1);
+
+                    copy_tile_init();
+                    copy_tile(cb_dy, 0, dst0);
+                    pack_tile(dst0, cb_add);
+
+                    cb_pop_front(cb_dy, onetile);
+                    cb_push_back(cb_add, onetile);
+
+                    release_dst(tt::DstMode::Half);
                 }
             }
         }
 
         // This reduce should not affect the result.
-        ACQ();
-        reduce_tile_to_cb(REDUCE_OP, REDUCE_DIM, cb_add, cb_bcast_scaler, cb_sum, 1, /*pop0=*/1, /*pop1=*/0);
-        REL();
+        acquire_dst(tt::DstMode::Half);
+
+        cb_reserve_back(cb_sum, onetile);
+        cb_wait_front(cb_bcast_scaler, onetile);
+
+        reduce_init_delta<false>(REDUCE_OP, REDUCE_DIM);
+        for (uint32_t x = 0; x < 1; ++x) {
+            cb_wait_front(cb_add, x + 1);  // must be a cumulative wait for correctness
+
+            constexpr uint32_t bcast_scaler0 = 0;  // 0th index from bcast_scaler CB
+            reduce_tile(cb_add, cb_bcast_scaler, x, bcast_scaler0, dst0);
+        }
+        cb_pop_front(cb_add, onetile);
+
+        reduce_revert_delta();
+        pack_tile(dst0, cb_sum);
+        cb_push_back(cb_sum, onetile);
+
+        release_dst(tt::DstMode::Half);
 
         // Only this for loop should affect the result.
         for (uint32_t h = 0; h < Ht; ++h) {
             constexpr auto cb_tmp0 = tt::CB::c_intermed0;
-            ACQ();
-            copy_tile_to_cb(cb_y, cb_tmp0);
-            REL();
+            acquire_dst(tt::DstMode::Half);
+
+            cb_reserve_back(cb_tmp0, onetile);
+            cb_wait_front(cb_y, 1);
+
+            copy_tile_init();
+            copy_tile(cb_y, 0, dst0);
+            pack_tile(dst0, cb_tmp0);
+
+            cb_pop_front(cb_y, onetile);
+            cb_push_back(cb_tmp0, onetile);
+
+            release_dst(tt::DstMode::Half);
 
             // dy - y
-            ACQ();
-            sub_tiles_to_cb(cb_dy, cb_tmp0, cb_dx);
-            REL();
+            acquire_dst(tt::DstMode::Half);
+
+            cb_reserve_back(cb_dx, onetile);
+            cb_wait_front(cb_dy, onetile);
+            cb_wait_front(cb_tmp0, onetile);
+
+            sub_tiles_init();
+            sub_tiles(cb_dy, cb_tmp0, 0, 0, dst0);
+
+            pack_tile(dst0, cb_dx);
+
+            cb_pop_front(cb_dy, onetile);
+            cb_pop_front(cb_tmp0, onetile);
+
+            cb_push_back(cb_dx, onetile);
+
+            release_dst(tt::DstMode::Half);
         }
 
         cb_pop_front(cb_sum, onetile);
