44fa18fcb064f13adfae7e87a5d8499cc07a0b0a
keerthana-r-mcw <keerthana.rajendran@multicorewareinc.com>
2024-05-14 14:25:03 +0000

    #8031: Add support for shallow unet 256x512 group=2

--- tests/ttnn/integration_tests/unet/test_ttnn_shallow_unet.py
+++ tests/ttnn/integration_tests/unet/test_ttnn_shallow_unet.py
@@ -23,11 +23,11 @@ import tt_lib.profiler as profiler
 import ttnn
 
 
-def update_ttnn_module_args(ttnn_module_args):
-    ttnn_module_args["use_1d_systolic_array"] = ttnn_module_args.in_channels < 256
+def update_ttnn_module_args(ttnn_module_args, groups):
+    ttnn_module_args["use_1d_systolic_array"] = ttnn_module_args.in_channels < 256 or groups > 1
 
 
-def create_custom_preprocessor(device, groups=1):
+def create_custom_preprocessor(device, groups=1, act_block_n=1, num_core_nhw=32):
     def custom_preprocessor(model, name, ttnn_module_args):
         parameters = {}
         if isinstance(model, UNet):
@@ -60,12 +60,20 @@ def create_custom_preprocessor(device, groups=1):
             ttnn_module_args.c1["conv_blocking_and_parallelization_config_override"] = (
                 {"act_block_h": 32}
                 if groups > 1
-                else ({"act_block_h": 5 * 32} if device.arch() == ttl.device.Arch.WORMHOLE_B0 else {"act_block_h": 64})
+                else (
+                    {"act_block_h": act_block_n * 32}
+                    if device.arch() == ttl.device.Arch.WORMHOLE_B0
+                    else {"act_block_h": 64}
+                )
             )
             ttnn_module_args.c1_2["conv_blocking_and_parallelization_config_override"] = (
                 {"act_block_h": 32}
                 if groups > 1
-                else ({"act_block_h": 5 * 32} if device.arch() == ttl.device.Arch.WORMHOLE_B0 else {"act_block_h": 64})
+                else (
+                    {"act_block_h": act_block_n * 32}
+                    if device.arch() == ttl.device.Arch.WORMHOLE_B0
+                    else {"act_block_h": 64}
+                )
             )
 
             ttnn_module_args.c2["math_fidelity"] = ttnn.MathFidelity.LoFi
@@ -108,8 +116,12 @@ def create_custom_preprocessor(device, groups=1):
             ttnn_module_args.c4_2["activation"] = "relu"  # Fuse relu with conv4
             ttnn_module_args.c4["deallocate_activation"] = True
             ttnn_module_args.c4_2["deallocate_activation"] = True
-            ttnn_module_args.c4["conv_blocking_and_parallelization_config_override"] = None
-            ttnn_module_args.c4_2["conv_blocking_and_parallelization_config_override"] = None
+            ttnn_module_args.c4["conv_blocking_and_parallelization_config_override"] = (
+                {"num_cores_nhw": num_core_nhw} if groups > 1 else None
+            )
+            ttnn_module_args.c4_2["conv_blocking_and_parallelization_config_override"] = (
+                {"num_cores_nhw": num_core_nhw} if groups > 1 else None
+            )
 
             ttnn_module_args.bnc["math_fidelity"] = ttnn.MathFidelity.LoFi
             ttnn_module_args.bnc_2["math_fidelity"] = ttnn.MathFidelity.LoFi
@@ -139,9 +151,15 @@ def create_custom_preprocessor(device, groups=1):
             ttnn_module_args.c5["deallocate_activation"] = True
             ttnn_module_args.c5_2["deallocate_activation"] = True
             ttnn_module_args.c5_3["deallocate_activation"] = True
-            ttnn_module_args.c5["conv_blocking_and_parallelization_config_override"] = None
-            ttnn_module_args.c5_2["conv_blocking_and_parallelization_config_override"] = None
-            ttnn_module_args.c5_3["conv_blocking_and_parallelization_config_override"] = None
+            ttnn_module_args.c5["conv_blocking_and_parallelization_config_override"] = (
+                {"num_cores_nhw": num_core_nhw} if groups > 1 else None
+            )
+            ttnn_module_args.c5_2["conv_blocking_and_parallelization_config_override"] = (
+                {"num_cores_nhw": num_core_nhw} if groups > 1 else None
+            )
+            ttnn_module_args.c5_3["conv_blocking_and_parallelization_config_override"] = (
+                {"num_cores_nhw": num_core_nhw} if groups > 1 else None
+            )
 
             ttnn_module_args.c6["math_fidelity"] = ttnn.MathFidelity.LoFi
             ttnn_module_args.c6["use_shallow_conv_variant"] = (
@@ -238,29 +256,29 @@ def create_custom_preprocessor(device, groups=1):
             conv8_2_weight, conv8_2_bias = fold_batch_norm2d_into_conv2d(model.c8_2, model.b8_2)
             conv8_3_weight, conv8_3_bias = fold_batch_norm2d_into_conv2d(model.c8_3, model.b8_3)
 
-            update_ttnn_module_args(ttnn_module_args.c1)
-            update_ttnn_module_args(ttnn_module_args.c1_2)
-            update_ttnn_module_args(ttnn_module_args.c2)
-            update_ttnn_module_args(ttnn_module_args.c2_2)
-            update_ttnn_module_args(ttnn_module_args.c3)
-            update_ttnn_module_args(ttnn_module_args.c3_2)
-            update_ttnn_module_args(ttnn_module_args.c4)
-            update_ttnn_module_args(ttnn_module_args.c4_2)
-            update_ttnn_module_args(ttnn_module_args.bnc)
-            update_ttnn_module_args(ttnn_module_args.bnc_2)
-            update_ttnn_module_args(ttnn_module_args.c5)
-            update_ttnn_module_args(ttnn_module_args.c5_2)
-            update_ttnn_module_args(ttnn_module_args.c5_3)
-            update_ttnn_module_args(ttnn_module_args.c6)
-            update_ttnn_module_args(ttnn_module_args.c6_2)
-            update_ttnn_module_args(ttnn_module_args.c6_3)
-            update_ttnn_module_args(ttnn_module_args.c7)
-            update_ttnn_module_args(ttnn_module_args.c7_2)
-            update_ttnn_module_args(ttnn_module_args.c7_3)
-            update_ttnn_module_args(ttnn_module_args.c8)
-            update_ttnn_module_args(ttnn_module_args.c8_2)
-            update_ttnn_module_args(ttnn_module_args.c8_3)
-            update_ttnn_module_args(ttnn_module_args.output_layer)
+            update_ttnn_module_args(ttnn_module_args.c1, groups)
+            update_ttnn_module_args(ttnn_module_args.c1_2, groups)
+            update_ttnn_module_args(ttnn_module_args.c2, groups)
+            update_ttnn_module_args(ttnn_module_args.c2_2, groups)
+            update_ttnn_module_args(ttnn_module_args.c3, groups)
+            update_ttnn_module_args(ttnn_module_args.c3_2, groups)
+            update_ttnn_module_args(ttnn_module_args.c4, groups)
+            update_ttnn_module_args(ttnn_module_args.c4_2, groups)
+            update_ttnn_module_args(ttnn_module_args.bnc, groups)
+            update_ttnn_module_args(ttnn_module_args.bnc_2, groups)
+            update_ttnn_module_args(ttnn_module_args.c5, groups)
+            update_ttnn_module_args(ttnn_module_args.c5_2, groups)
+            update_ttnn_module_args(ttnn_module_args.c5_3, groups)
+            update_ttnn_module_args(ttnn_module_args.c6, groups)
+            update_ttnn_module_args(ttnn_module_args.c6_2, groups)
+            update_ttnn_module_args(ttnn_module_args.c6_3, groups)
+            update_ttnn_module_args(ttnn_module_args.c7, groups)
+            update_ttnn_module_args(ttnn_module_args.c7_2, groups)
+            update_ttnn_module_args(ttnn_module_args.c7_3, groups)
+            update_ttnn_module_args(ttnn_module_args.c8, groups)
+            update_ttnn_module_args(ttnn_module_args.c8_2, groups)
+            update_ttnn_module_args(ttnn_module_args.c8_3, groups)
+            update_ttnn_module_args(ttnn_module_args.output_layer, groups)
 
             parameters["c1"], c1_parallel_config = preprocess_conv2d(
                 conv1_weight, conv1_bias, ttnn_module_args.c1, return_parallel_config=True
@@ -513,7 +531,7 @@ class UNet(nn.Module):
 @skip_for_grayskull()
 @pytest.mark.parametrize("device_l1_small_size", [32768], indirect=True)
 @pytest.mark.parametrize("loop", [0])
-@pytest.mark.parametrize("perf_mode, groups", [(False, 1), (True, 1)])  # , (True, 2)])
+@pytest.mark.parametrize("perf_mode, groups", [(False, 1), (True, 1), (True, 2)])  # , (True, 4)])
 def test_unet(device, loop, perf_mode, groups):
     if perf_mode and device.arch() == ttl.device.Arch.GRAYSKULL:
         pytest.skip("Perf mode is not supported on Grayskull")
@@ -547,7 +565,104 @@ def test_unet(device, loop, perf_mode, groups):
         parameters = preprocess_model(
             initialize_model=lambda: torch_model,
             run_model=lambda model: model(torch_input_tensor),
-            custom_preprocessor=create_custom_preprocessor(device, groups=groups),
+            custom_preprocessor=create_custom_preprocessor(device, groups=groups, act_block_n=5, num_core_nhw=42),
+            reader_patterns_cache=reader_patterns_cache,
+            device=device,
+        )
+
+        ttnn_model = ttnn_shallow_unet.UNet(parameters)
+
+        #
+        # Tensor Preprocessing
+        #
+        input_shape = torch_input_tensor.shape
+        input_tensor = torch.permute(torch_input_tensor, (0, 2, 3, 1))
+
+        # Pad to 16 if grayskull run and 32 for wormhole
+        input_tensor = input_tensor.reshape(
+            input_tensor.shape[0], 1, input_tensor.shape[1] * input_tensor.shape[2], input_tensor.shape[3]
+        )
+        pad = 32 if device.arch() == ttl.device.Arch.WORMHOLE_B0 else 16
+        hpad = 0  # 96*32*64
+        if input_tensor.shape[-1] < pad or input_tensor.shape[-2] < hpad:
+            input_tensor = torch.nn.functional.pad(
+                input_tensor, (0, max(0, pad - input_tensor.shape[-1]), 0, max(0, hpad - input_tensor.shape[-2]))
+            )
+        input_tensor = ttnn.from_torch(input_tensor, dtype=ttnn.bfloat16)
+
+        warmup = 1
+        start = None
+        for i in range(loop + warmup):
+            if i == warmup:
+                start = time.perf_counter()
+            profiler.tracy_frame()
+            output_tensor = ttnn_model(device, input_tensor, original_shape, perf_mode=perf_mode)
+        if start is not None:
+            stop = time.perf_counter()
+            total_time = stop - start
+            batch = input_shape[0]
+            total_frame_count = batch * loop
+            print(f"Elapsed host time (sec): {total_time}")
+            print(f"Frames processed: {total_frame_count}")
+            print(f"Host perf (fps): {total_frame_count / total_time}")
+
+        #
+        # Tensor Postprocessing
+        #
+        output_tensor = ttnn.to_torch(output_tensor)
+        output_tensor = output_tensor[:, :, :, :groups]
+        output_tensor = output_tensor.reshape(input_shape[0], input_shape[2], input_shape[3], -1)
+        output_tensor = torch.permute(output_tensor, (0, 3, 1, 2))
+        output_tensor = output_tensor.to(torch_input_tensor.dtype)
+
+        if perf_mode:
+            pass  # skip PCC checks if perf_mode is set
+        elif device.arch() == ttl.device.Arch.WORMHOLE_B0:
+            assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+        else:
+            assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.97)
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_l1_small_size", [32768], indirect=True)
+@pytest.mark.parametrize("loop", [0])
+@pytest.mark.parametrize("perf_mode, groups", [(False, 1), (True, 1), (True, 2)])
+def test_unet_512x256(device, loop, perf_mode, groups):
+    if perf_mode and device.arch() == ttl.device.Arch.GRAYSKULL:
+        pytest.skip("Perf mode is not supported on Grayskull")
+
+    if is_x2_harvested(device):
+        pytest.skip("x2 harvested chip is not supported")
+
+    with torch.no_grad():
+        torch.manual_seed(0)
+        torch_model = UNet(groups=groups)
+        torch_model.eval()
+
+        new_state_dict = {}
+        for name, parameter in torch_model.state_dict().items():
+            if isinstance(parameter, torch.FloatTensor):
+                if "b1" or "b2" or "b3" or "b4" or "bnb" in name:
+                    new_state_dict[name] = parameter
+                else:
+                    new_state_dict[name] = parameter + 1000
+
+        torch_model.load_state_dict(new_state_dict)
+
+        batch = 1 if groups > 1 else 2
+        torch_input_tensor = torch.randn(
+            batch, 4 * groups, 512, 256
+        )  # Batch size of 2, 3 channels (RGB), 512x256 input
+        original_shape = list(torch_input_tensor.shape)
+        torch_output_tensor = torch_model(torch_input_tensor)
+
+        reader_patterns_cache = {}
+        parameters = preprocess_model(
+            initialize_model=lambda: torch_model,
+            run_model=lambda model: model(torch_input_tensor),
+            custom_preprocessor=create_custom_preprocessor(
+                device, groups=groups, act_block_n=8, num_core_nhw=64
+            ),  # act_block_n = input_width/32
             reader_patterns_cache=reader_patterns_cache,
             device=device,
         )
@@ -615,4 +730,5 @@ if __name__ == "__main__":
     device_id = 0
     device = ttnn.open_device(device_id=device_id)
     test_unet(device, args.loop, args.perf_mode, args.groups)
+    test_unet_512x256(device, args.loop, args.perf_mode, args.groups)
     ttnn.close_device(device)
