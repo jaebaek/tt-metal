f9b636bbd3785c727160145653a29bb75182d1a9
Vincent Tang <vtang@tenstorrent.com>
2024-05-22 18:53:34 +0000

    #8729: initial pytest smi reset infra

--- conftest.py
+++ conftest.py
@@ -12,6 +12,7 @@ from itertools import chain
 from operator import contains, eq, getitem
 from pathlib import Path
 import json
+import subprocess as sp
 
 from loguru import logger
 
@@ -68,191 +69,6 @@ def get_tt_cache_path():
             return default_path
 
     return get_tt_cache_path_
-
-
-ALL_ARCHS = set(
-    [
-        "grayskull",
-        "wormhole_b0",
-    ]
-)
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--tt-arch",
-        choices=[*ALL_ARCHS],
-        default=os.environ.get("ARCH_NAME", "grayskull"),
-        help="Target arch, ex. grayskull, wormhole_b0",
-    )
-    parser.addoption(
-        "--device-id",
-        type=int,
-        default=0,
-        help="Target device id",
-    )
-    parser.addoption(
-        "--input-method",
-        action="store",
-        choices=["json", "cli"],
-        default=None,
-        help="Choose input method: 1) json or 2) cli",
-    )
-    parser.addoption(
-        "--input-path",
-        action="store",
-        default="",
-        help="Path to json file with inputs",
-    )
-    parser.addoption("--cli-input", action="store", default=None, help="Enter prompt if --input-method=cli")
-
-
-def pytest_generate_tests(metafunc):
-    """
-    This is not a standard docstring.
-
-    We will explain the non-standard fixtures that pytest_generate_tests is
-    creating here.
-
-    silicon_arch_name and silicon_arch_<ARCH_NAME>
-    ----------------------------------------------
-
-    This is how tests should be requesting accelerator architecture names.
-    Tests which aim to run on silicon should request a silicon_arch_name
-    fixture. Just that single fixture will parametrize the test to run on the
-    provided architecture name from the command line through the --tt-arch
-    option. The value of the fixture will be the string value of the
-    architecture name. For example,
-
-    @pytest.mark.post_commit
-    def test_model_silicon(silicon_arch_name):
-        # silicon_arch_name will be one of grayskull, wormhole_b0 etc.
-        run_model_on_silicon(silicon_arch_name)
-        ...
-
-    If you want to restrict a test to only a specific architecture, you can
-    provide an additional fixture in the form of silicon_arch_<ARCH_NAME>. This
-    will limit the range of possible values for silicon_arch_name to only be
-    ARCH_NAME.
-
-    @pytest.mark.post_commit
-    def test_model_silicon_grayskull_only(
-        silicon_arch_name,
-        silicon_arch_grayskull,
-    ):
-        # silicon_arch_name can only be grayskull or empty
-        run_model_on_silicon(silicon_arch_name)
-        ...
-
-    If --tt-arch specifies an architecture that's not ARCH_NAME, the test will
-    be skipped. We ensure skipping by providing an empty list parametrization
-    for silicon_arch_name, and with the empty_parameter_set_mark config option
-    for pytest, will skip any tests with an empty list parametrization.
-
-    Note that you must provide silicon_arch_name as a fixture if you want to
-    use the silicon_arch_<ARCH_NAME> fixture.
-
-    Note that if tests want to use the ARCH value from the API, tests should
-    create their own separate fixture which will convert the string value
-    provided from silicon_arch_name into ARCH. We keep it as strings here
-    because these fixtures will be used in tests which do not have access to
-    any Python APIs.
-    """
-
-    tt_arch = metafunc.config.getoption("--tt-arch")
-
-    silicon_arch_specific_fixture_name_to_avail_archs = {
-        "silicon_arch_grayskull": set(
-            [
-                "grayskull",
-            ]
-        ),
-        "silicon_arch_wormhole_b0": set(
-            [
-                "wormhole_b0",
-            ]
-        ),
-    }
-
-    check_uses_silicon_arch_specific_fixture = partial(contains, silicon_arch_specific_fixture_name_to_avail_archs)
-    test_requested_silicon_arch_fixtures = tuple(
-        filter(check_uses_silicon_arch_specific_fixture, metafunc.fixturenames)
-    )
-    is_test_requesting_specific_silicon_archs = len(test_requested_silicon_arch_fixtures) > 0
-    get_archs_for_silicon_arch_specific_fixture = partial(getitem, silicon_arch_specific_fixture_name_to_avail_archs)
-    test_requested_silicon_archs = ALL_ARCHS.intersection(
-        *map(
-            get_archs_for_silicon_arch_specific_fixture,
-            test_requested_silicon_arch_fixtures,
-        )
-    )
-
-    available_archs = test_requested_silicon_archs if is_test_requesting_specific_silicon_archs else ALL_ARCHS
-    matches_user_requested_silicon_arch = partial(eq, tt_arch)
-    available_archs = tuple(filter(matches_user_requested_silicon_arch, available_archs))
-
-    uses_silicon_arch = "silicon_arch_name" in metafunc.fixturenames
-
-    # sanity
-    if is_test_requesting_specific_silicon_archs and not uses_silicon_arch:
-        raise Exception(
-            f"{metafunc.function} requesting a specific silicon target, but doesn't use silicon_arch_name fixture"
-        )
-
-    if uses_silicon_arch:
-        metafunc.parametrize("silicon_arch_name", available_archs)
-        for test_requested_silicon_arch_fixture in test_requested_silicon_arch_fixtures:
-            # The values of these arch-specific fixtures should not be used in
-            # the test function, so use any parameters, like [True]
-            metafunc.parametrize(test_requested_silicon_arch_fixture, [True])
-
-    input_method = metafunc.config.getoption("--input-method")
-    if input_method == "json":
-        json_path = metafunc.config.getoption("--input-path")
-        if not json_path:
-            raise ValueError("Please provide a valid JSON path using --input-path option.")
-        with open(json_path, "r") as f:
-            data = json.load(f)
-        metafunc.parametrize("user_input", [data])
-    elif input_method == "cli":
-        cli_input = metafunc.config.getoption("--cli-input")
-        if not cli_input:
-            raise ValueError("Please provide input using --cli-input option.")
-        metafunc.parametrize("user_input", [[cli_input]])
-
-
-# Report stashing to get outcomes etc
-phase_report_key = pytest.StashKey()
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    # execute all other hooks to obtain the report object
-    outcome = yield
-    rep = outcome.get_result()
-
-    # store test results for each phase of a call, which can
-    # be "setup", "call", "teardown"
-    item.stash.setdefault(phase_report_key, {})[rep.when] = rep
-
-
-@pytest.fixture(scope="function")
-def reset_tensix(request, silicon_arch_name):
-    yield
-
-    report = request.node.stash[phase_report_key]
-
-    test_failed = ("call" not in report) or report["call"].failed
-
-    if test_failed:
-        logger.debug("Test failed - resetting with smi")
-        if silicon_arch_name == "grayskull":
-            result = run_process_and_get_result("tt-smi -tr all")
-        elif silicon_arch_name == "wormhole_b0":
-            result = run_process_and_get_result("tt-smi -wr all")
-        else:
-            raise Exception(f"Unrecognized arch for tensix-reset: {silicon_arch_name}")
-        assert result.returncode == 0, "Tensix reset script raised error"
 
 
 @pytest.fixture(scope="function")
@@ -442,6 +258,241 @@ def tracy_profile():
     profiler.disable()
 
 
-@pytest.fixture
-def input_path(request):
-    return request.config.getoption("--input-path")
+###############################
+# Modifying pytest hooks
+###############################
+ALL_ARCHS = set(
+    [
+        "grayskull",
+        "wormhole_b0",
+    ]
+)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--tt-arch",
+        choices=[*ALL_ARCHS],
+        default=os.environ.get("ARCH_NAME", "grayskull"),
+        help="Target arch, ex. grayskull, wormhole_b0",
+    )
+    parser.addoption(
+        "--pipeline-type",
+        default="",
+        help="Only `models_device_performance_bare_metal` should run `pytest_runtest_teardown`",
+    )
+    parser.addoption(
+        "--device-id",
+        type=int,
+        default=0,
+        help="Target device id",
+    )
+    parser.addoption(
+        "--input-method",
+        action="store",
+        choices=["json", "cli"],
+        default=None,
+        help="Choose input method: 1) json or 2) cli",
+    )
+    parser.addoption(
+        "--input-path",
+        action="store",
+        default="",
+        help="Path to json file with inputs",
+    )
+    parser.addoption("--cli-input", action="store", default=None, help="Enter prompt if --input-method=cli")
+
+
+def pytest_generate_tests(metafunc):
+    """
+    This is not a standard docstring.
+
+    We will explain the non-standard fixtures that pytest_generate_tests is
+    creating here.
+
+    silicon_arch_name and silicon_arch_<ARCH_NAME>
+    ----------------------------------------------
+
+    This is how tests should be requesting accelerator architecture names.
+    Tests which aim to run on silicon should request a silicon_arch_name
+    fixture. Just that single fixture will parametrize the test to run on the
+    provided architecture name from the command line through the --tt-arch
+    option. The value of the fixture will be the string value of the
+    architecture name. For example,
+
+    @pytest.mark.post_commit
+    def test_model_silicon(silicon_arch_name):
+        # silicon_arch_name will be one of grayskull, wormhole_b0 etc.
+        run_model_on_silicon(silicon_arch_name)
+        ...
+
+    If you want to restrict a test to only a specific architecture, you can
+    provide an additional fixture in the form of silicon_arch_<ARCH_NAME>. This
+    will limit the range of possible values for silicon_arch_name to only be
+    ARCH_NAME.
+
+    @pytest.mark.post_commit
+    def test_model_silicon_grayskull_only(
+        silicon_arch_name,
+        silicon_arch_grayskull,
+    ):
+        # silicon_arch_name can only be grayskull or empty
+        run_model_on_silicon(silicon_arch_name)
+        ...
+
+    If --tt-arch specifies an architecture that's not ARCH_NAME, the test will
+    be skipped. We ensure skipping by providing an empty list parametrization
+    for silicon_arch_name, and with the empty_parameter_set_mark config option
+    for pytest, will skip any tests with an empty list parametrization.
+
+    Note that you must provide silicon_arch_name as a fixture if you want to
+    use the silicon_arch_<ARCH_NAME> fixture.
+
+    Note that if tests want to use the ARCH value from the API, tests should
+    create their own separate fixture which will convert the string value
+    provided from silicon_arch_name into ARCH. We keep it as strings here
+    because these fixtures will be used in tests which do not have access to
+    any Python APIs.
+    """
+
+    tt_arch = metafunc.config.getoption("--tt-arch")
+
+    silicon_arch_specific_fixture_name_to_avail_archs = {
+        "silicon_arch_grayskull": set(
+            [
+                "grayskull",
+            ]
+        ),
+        "silicon_arch_wormhole_b0": set(
+            [
+                "wormhole_b0",
+            ]
+        ),
+    }
+
+    check_uses_silicon_arch_specific_fixture = partial(contains, silicon_arch_specific_fixture_name_to_avail_archs)
+    test_requested_silicon_arch_fixtures = tuple(
+        filter(check_uses_silicon_arch_specific_fixture, metafunc.fixturenames)
+    )
+    is_test_requesting_specific_silicon_archs = len(test_requested_silicon_arch_fixtures) > 0
+    get_archs_for_silicon_arch_specific_fixture = partial(getitem, silicon_arch_specific_fixture_name_to_avail_archs)
+    test_requested_silicon_archs = ALL_ARCHS.intersection(
+        *map(
+            get_archs_for_silicon_arch_specific_fixture,
+            test_requested_silicon_arch_fixtures,
+        )
+    )
+
+    available_archs = test_requested_silicon_archs if is_test_requesting_specific_silicon_archs else ALL_ARCHS
+    matches_user_requested_silicon_arch = partial(eq, tt_arch)
+    available_archs = tuple(filter(matches_user_requested_silicon_arch, available_archs))
+
+    uses_silicon_arch = "silicon_arch_name" in metafunc.fixturenames
+
+    # sanity
+    if is_test_requesting_specific_silicon_archs and not uses_silicon_arch:
+        raise Exception(
+            f"{metafunc.function} requesting a specific silicon target, but doesn't use silicon_arch_name fixture"
+        )
+
+    if uses_silicon_arch:
+        metafunc.parametrize("silicon_arch_name", available_archs)
+        for test_requested_silicon_arch_fixture in test_requested_silicon_arch_fixtures:
+            # The values of these arch-specific fixtures should not be used in
+            # the test function, so use any parameters, like [True]
+            metafunc.parametrize(test_requested_silicon_arch_fixture, [True])
+
+    input_method = metafunc.config.getoption("--input-method")
+    if input_method == "json":
+        json_path = metafunc.config.getoption("--input-path")
+        if not json_path:
+            raise ValueError("Please provide a valid JSON path using --input-path option.")
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        metafunc.parametrize("user_input", [data])
+    elif input_method == "cli":
+        cli_input = metafunc.config.getoption("--cli-input")
+        if not cli_input:
+            raise ValueError("Please provide input using --cli-input option.")
+        metafunc.parametrize("user_input", [[cli_input]])
+
+
+# Report stashing to get outcomes etc
+phase_report_key = pytest.StashKey()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # store test results for each phase of a call, which can
+    # be "setup", "call", "teardown"
+    item.stash.setdefault(phase_report_key, {})[rep.when] = rep
+    # call_report = item.stash.get(phase_report_key, {}).get('call')
+
+    # if call_report is not None and call_report.failed:
+    #     logger.info('Test failed.')
+
+    # if call.when == 'call':
+    #     if call.excinfo is not None:
+    #         # The test has failed.
+    #         item._test_failed = True
+    # elif call.when == 'teardown':
+    #     if hasattr(item, '_test_failed'):
+    #         logger.debug('Test failed. Resetting with smi')
+    # run_process_and_get_result("tt-smi -tr all", {}, False)
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_teardown(item, nextitem):
+    yield
+    pipeline_type = item.config.getoption("--pipeline-type")
+    valid_pipelines = ["models_device_performance_bare_metal"]
+    if pipeline_type in valid_pipelines:
+        logger.debug(f"Running custom teardown")
+        report = item.stash[phase_report_key]
+        test_failed = report.get("call", None) and report["call"].failed
+        if test_failed:
+            arch = item.config.getoption("--tt-arch")
+            check_smi = run_process_and_get_result("tt-smi-metal")
+            # logger.debug(f"check smi: {check_smi.returncode}")
+            if check_smi.returncode > 0:
+                logger.debug(f"Test failed - resetting {arch} with tt-smi")
+                if arch == "grayskull":
+                    smi_reset_result = run_process_and_get_result("tt-smi -tr all")
+                elif arch == "wormhole_b0":
+                    smi_reset_result = run_process_and_get_result("tt-smi -wr all")
+                else:
+                    raise Exception(f"Unrecognized arch for tensix-reset: {arch}")
+            else:
+                logger.debug(f"Test failed - resetting {arch} with tt-smi-metal script")
+                if arch != "grayskull" and arch != "wormhole_b0":
+                    raise Exception(f"Unrecognized arch for tensix-reset: {arch}")
+                else:
+                    smi_reset_result = run_process_and_get_result(f"./opt/tt_metal_infra/scripts/ci/{arch}/reset.sh")
+            logger.debug(f"tt-smi reset status: {smi_reset_result.returncode}")
+
+
+# @pytest.fixture(scope="function")
+# def reset_tensix(request, silicon_arch_name):
+#     yield
+
+#     report = request.node.stash[phase_report_key]
+
+#     test_failed = ("call" not in report) or report["call"].failed
+
+#     if test_failed:
+#         logger.debug("Test failed - resetting with smi")
+#         if silicon_arch_name == "grayskull":
+#             result = run_process_and_get_result("tt-smi -tr all")
+#         elif silicon_arch_name == "wormhole_b0":
+#             result = run_process_and_get_result("tt-smi -wr all")
+#         else:
+#             raise Exception(f"Unrecognized arch for tensix-reset: {silicon_arch_name}")
+#         assert result.returncode == 0, "Tensix reset script raised error"
+
+# @pytest.fixture
+# def input_path(request):
+#     return request.config.getoption("--input-path")
