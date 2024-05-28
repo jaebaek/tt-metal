348ba4206222fa45df46e2502276dd154106c028
Mark O'Connor <moconnor@tenstorrent.com>
2024-05-28 12:29:59 +0000

    #8895: Add test for as_tensor with device_mesh (fails)

--- tests/ttnn/unit_tests/test_as_tensor.py
+++ tests/ttnn/unit_tests/test_as_tensor.py
@@ -55,3 +55,41 @@ def test_as_tensor_with_cache(tmp_path, device, height, width):
 
     torch_output_tensor = ttnn.to_torch(tensor)
     assert torch.allclose(torch_input_tensor, torch_output_tensor)
+
+
+@pytest.mark.parametrize("height", [7])
+@pytest.mark.parametrize("width", [3])
+def test_as_tensor_with_device_mesh(tmp_path, device_mesh, height, width):
+    torch_input_tensor = torch.rand((height, width), dtype=torch.float32)
+
+    memory_config = ttnn.L1_MEMORY_CONFIG
+    tensor = ttnn.as_tensor(
+        torch_input_tensor,
+        dtype=ttnn.float32,
+        layout=ttnn.TILE_LAYOUT,
+        device=device_mesh,
+        memory_config=memory_config,
+        mesh_mapper=ttnn.ReplicateTensorToMesh(device_mesh),
+        cache_file_name=tmp_path / "cache_file",
+    )
+    assert tensor.dtype == ttnn.float32
+    assert tensor.device() in device_mesh.get_devices()
+    assert tensor.layout == ttnn.TILE_LAYOUT
+    assert ttnn.get_memory_config(tensor) == memory_config
+
+    tensor = ttnn.as_tensor(
+        torch_input_tensor,
+        dtype=ttnn.float32,
+        layout=ttnn.TILE_LAYOUT,
+        device=device_mesh,
+        memory_config=memory_config,
+        mesh_mapper=ttnn.ReplicateTensorToMesh(device_mesh),
+        cache_file_name=tmp_path / "cache_file",
+    )
+    assert tensor.dtype == ttnn.float32
+    assert tensor.device() in device_mesh.get_devices()
+    assert tensor.layout == ttnn.TILE_LAYOUT
+    assert ttnn.get_memory_config(tensor) == memory_config
+
+    torch_output_tensor = ttnn.to_torch(tensor)
+    assert torch.allclose(torch_input_tensor, torch_output_tensor)
